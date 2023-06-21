f518cf683712581cb87ec4360bb33e2cf19ff32f
Pablo Soares <115740364+Pablosoares22@users.noreply.github.com>
2023-06-21 14:51:32 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const menuHamburguer = document.querySelector('.hamburguer-menu');
+
+const menu = document.querySelector('.menu');
+
+menuHamburguer.addEventListener('click', () => { 
+     menu.classList.toggle('ativo');
+})
+  
